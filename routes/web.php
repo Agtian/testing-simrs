cb243b995c0da2e0f1d2d51babea346098b0584b
agtian <agtianrickey33@gmail.com>
2022-04-27 13:46:34 +0700

    integration template Admin LTE

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,29 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
+
+// Penggunaan prefix group
+// Route::prefix('master-data')->group(function () {
+//     Route::get('/data-karyawan', [CrudController::class, 'index'])->name('dashboard');
+//     Route::get('/data-kerja', [CrudController::class, 'edit'])->name('dashboard');
+// });
+// Route 1 : Maka pada view pemanggilannya = /master-data.data-karyawan atau route('dashboard')
+// Route 2 : Maka pada view pemanggilannya = /master-data.data-kerja atau route('dashboard-edit')
+
+// Penggunaan name group
+// Route::name('master-data.')->group(function () {
+//     Route::get('master-data/data-karyawan', [CrudController::class, 'index'])->name('data-karyawan');
+//     Route::get('master-data/data-kerja', [CrudController::class, 'edit'])->name('data-kerja');
+// });
+// Route 1 : Maka pada view pemanggilannya = /master-data.data-karyawan atau route('master-data.data-karyawan')
+// Route 2 : Maka pada view pemanggilannya = /master-data.data-kerja atau route('master-data.data-kerja')
+
+// Penggunaan prefix group dan name group
+// Route::name('master-data.')->prefix('master-data')->group(function () {
+//     Route::get('/data-karyawan', [CrudController::class, 'index'])->name('data-karyawan');
+//     Route::get('/data-kerja', [CrudController::class, 'edit'])->name('data-kerja');
+// });
+// Route 1 : Maka pada view pemanggilannya = /master-data.data-karyawan atau route('master-data.data-karyawan')
+// Route 2 : Maka pada view pemanggilannya = /master-data.data-kerja atau route('master-data.data-kerja')
