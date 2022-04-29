daff23a70cccbcec254e439c4ecc6a884cb20d65
agtian <agtianrickey33@gmail.com>
2022-04-29 21:15:23 +0700

    fix error redirect on Socialite

--- routes/web.php
+++ routes/web.php
@@ -26,10 +26,10 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index']);
 });
 
-// Route::get('/auth/google', [GoogleController::class, 'redirectToGoogle'])->name('google.login');
-Route::get('/auth/google', function () {
-    return Socialite::driver('google')->redirect();
-});
+Route::get('auth/google', [GoogleController::class, 'redirectToGoogle'])->name('google.login');
+// Route::get('/auth/google', function () {
+//     return Socialite::driver('google')->redirect();
+// });
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
